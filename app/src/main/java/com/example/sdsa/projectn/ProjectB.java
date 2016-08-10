51c5ad7ec2862e98094574f52a4aeb010fecaa77
janechoe <choejane95@gmail.com>
2016-08-10 10:11:38 -0400

    Project B files added and committed

---
+++ app/src/main/java/com/example/sdsa/projectn/ProjectB.java
@@ -0,0 +1,13 @@
+package com.example.sdsa.projectn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProjectB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_b);
+    }
+}
